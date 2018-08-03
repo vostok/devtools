2e2a4cc734c4f7124bac08e29106461293bdd123
Krait <krait@skbkontur.ru>
2018-08-03 19:56:18 +0500

    Added a sample project for source libraries.

---
+++ src-library-project-sample/Vostok.SampleLibrary/SampleClass.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+
+namespace Vostok.SampleLibrary
+{
+    /// <summary>
+    /// A sample class.
+    /// </summary>
+    [PublicAPI]
+#if MAKE_CLASSES_PUBLIC
+    public
+#else
+    internal
+#endif
+    class SampleClass
+    {
+        
+    }
+}
