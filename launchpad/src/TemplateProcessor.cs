2f755611ec11454ea224f33790f9c4a0940bd9b0
d.lukshto@skbkontur.ru <d.lukshto@skbkontur.ru>
2018-08-11 17:09:01 +0500

    implement TemplateProcessor

--- launchpad/src/TemplateProcessor.cs
+++ launchpad/src/TemplateProcessor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using Stubble.Core.Builders;
 
 namespace launchpad
 {
@@ -8,7 +9,31 @@ namespace launchpad
     {
         public void Process(DirectoryInfo directory, Dictionary<string, string> variables)
         {
-            throw new NotImplementedException();
+            var stubble = new StubbleBuilder().Build();
+
+            var stack = new Stack<FileSystemInfo>();
+
+            stack.Push(directory);
+            while (stack.Count > 0)
+            {
+                var sysInfo = stack.Pop();
+                var directoryInfo = sysInfo as DirectoryInfo;
+                if (directoryInfo != null)
+                {   
+                    directoryInfo.MoveTo(stubble.Render(directory.Name, variables));
+                    foreach (var systemInfo in directoryInfo.EnumerateFileSystemInfos())
+                    {
+                        stack.Push(systemInfo);
+                    }
+                }
+                var fileInfo = sysInfo as FileInfo;
+                if (fileInfo != null)
+                {
+                    fileInfo.MoveTo(stubble.Render(directory.Name, variables));
+                    stubble.Render(File.ReadAllText(fileInfo.DirectoryName), variables);
+                    
+                }
+            }
         }
     }
 }
