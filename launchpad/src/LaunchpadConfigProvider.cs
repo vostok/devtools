64cb5b79b8e06675d1d5efe47d23cf18f4db5b39
iloktionov <iloktionov@skbkontur.ru>
2018-08-13 12:42:37 +0500

    LaunchpadConfigProvider now supports custom config path via environment variable.

--- launchpad/src/LaunchpadConfigProvider.cs
+++ launchpad/src/LaunchpadConfigProvider.cs
@@ -6,9 +6,29 @@ namespace launchpad
 {
     internal class LaunchpadConfigProvider
     {
+        private const string DefaultConfigFileName = "launchpad-config.json";
+
+        private const string EnvironmentVariableName = "LAUNCHPAD_CONFIG_PATH";
+
+        public void SetupConfigPath(string path)
+        {
+            Environment.SetEnvironmentVariable(EnvironmentVariableName, path, EnvironmentVariableTarget.User);
+        }
+
         public LaunchpadConfig GetConfig()
         {
-            var fileContent = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "launchpad-config.json"));
+            var pathFromEnvironment = Environment.GetEnvironmentVariable(EnvironmentVariableName);
+
+            string fileContent;
+
+            if (!string.IsNullOrEmpty(pathFromEnvironment) && File.Exists(pathFromEnvironment))
+            {
+                fileContent = File.ReadAllText(pathFromEnvironment);
+            }
+            else
+            {
+                fileContent = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, DefaultConfigFileName));
+            }
 
             return JsonConvert.DeserializeObject<LaunchpadConfig>(fileContent);
         }
