7c64be814e17daef7b499b16393a4100acaa0289
epeshk <peshkov@skbkontur.ru>
2018-09-06 15:24:34 +0500

    update library-source project template for new publicize convention

--- launchpad-templates/library-source/template/{{ProjectName}}/SampleClass.cs
+++ launchpad-templates/library-source/template/{{ProjectName}}/SampleClass.cs
@@ -6,12 +6,7 @@ namespace {{ProjectName}}
     /// A sample class.
     /// </summary>
     [PublicAPI]
-#if MAKE_CLASSES_PUBLIC
-    public
-#else
-    internal
-#endif
-    class SampleClass
+    internal class SampleClass
     {
         
     }
